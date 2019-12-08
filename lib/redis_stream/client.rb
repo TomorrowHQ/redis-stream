e11029b5f539837fce31f10867e907108af4a43b
Anton <choixer@gmail.com>
2019-12-09 00:40:26 +0100

    Move stream logic into separate class

--- lib/redis_stream/client.rb
+++ lib/redis_stream/client.rb
@@ -1,36 +1,11 @@
 module RedisStream
   class Client
-    PREFIX = 'stream-'.freeze
-
     def initialize
       @redis = Redis.new
     end
 
-    def add(entry, key:)
-      @redis.xadd(key, value: entry)
-    end
-
-    def len(key:)
-      @redis.xlen(key)
-    end
-
-    def clear(key:)
-      @redis.xtrim(key, 0)
-    end
-
-    def each_message(key:)
-      last_id = '0'
-
-      while
-        result = @redis.xread(key, last_id, count: 1)
-        break if result.empty?
-
-        messages = result[key]
-        messages.each do |message|
-          last_id, entry = message
-          yield(entry['value'])
-        end
-      end
+    def stream(key)
+      Stream.new(redis: @redis, key: key)
     end
 
     def group(key:, name:)
--- test/redis_stream_test.rb
+++ test/redis_stream_test.rb
@@ -1,48 +1,32 @@
 require 'test_helper'
 
 describe RedisStream do
-  before(:each) do
-    @stream = RedisStream.new
-    @stream.group(key: 'test-1', name: 'testers').destroy
-    @stream.clear(key: 'test-1')
-    @stream.clear(key: 'test-2')
+  before do
+    @client = RedisStream.new
   end
 
   it 'defines a version' do
     refute_nil ::RedisStream::VERSION
   end
 
-  it 'puts messages in redis stream' do
-    @stream.add('Message', key: 'test-1')
-    assert_equal 1, @stream.len(key: 'test-1')
-    assert_equal 0, @stream.len(key: 'test-2')
-
-    @stream.add('Message', key: 'test-2')
-    @stream.add('Message', key: 'test-2')
-    assert_equal 2, @stream.len(key: 'test-2')
-    assert_equal 1, @stream.len(key: 'test-1')
-  end
-
-  it 'reads messages from the begining of stream' do
-    @stream.add('Message 1', key: 'test-1')
-    @stream.add('Message 2', key: 'test-1')
-
-    received_messages = []
-    @stream.each_message(key: 'test-1') do |message|
-      received_messages << message
+  describe '#stream' do
+    it 'returns stream' do
+      stream = @client.stream('test')
+      assert_kind_of RedisStream::Stream, stream
+      assert_equal 'test', stream.key
     end
-
-    assert_equal ['Message 1', 'Message 2'], received_messages
   end
 
-  it 'creates consumer group' do
-    group = @stream.group(key: 'test-1', name: 'testers')
-    assert_kind_of RedisStream::Group, group
+  describe '#group' do
+    it 'creates consumer group' do
+      group = @client.group(key: 'test-1', name: 'testers')
+      assert_kind_of RedisStream::Group, group
 
-    same_group = @stream.group(key: 'test-1', name: 'testers')
-    assert_kind_of RedisStream::Group, same_group
+      same_group = @client.group(key: 'test-1', name: 'testers')
+      assert_kind_of RedisStream::Group, same_group
 
-    assert_equal group.key, same_group.key
-    assert_equal group.name, same_group.name
+      assert_equal group.key, same_group.key
+      assert_equal group.name, same_group.name
+    end
   end
 end
